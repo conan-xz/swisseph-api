471467180047af84f8d8db12619ca1e3781b9e5e
conan <zknn@sina.com>
2026-02-10 15:48:38 +0800

    --

--- config/wechat.js
+++ config/wechat.js
@@ -4,8 +4,8 @@
  */
 
 // Get from environment variables or use defaults
-const WECHAT_APP_ID = process.env.WECHAT_APP_ID || 'wx3d738eee512524be';
-const WECHAT_APP_SECRET = process.env.WECHAT_APP_SECRET || '28343d3cff5bc9e79837bc0391916f0a';
+const WECHAT_APP_ID = process.env.WECHAT_APP_ID || '';
+const WECHAT_APP_SECRET = process.env.WECHAT_APP_SECRET || '';
 
 module.exports = {
   appId: WECHAT_APP_ID,
