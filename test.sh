00959b1fe26eccb01b096faaa05b33698aaf8a5a
conan <zknn@sina.com>
2026-02-24 15:31:57 +0800

    删除测试脚本

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Birth Chart Generator Test
-# Usage: ./test.sh [url]
-
-SERVICE_URL="${1:-https://express-v2qc-140092-7-1342501112.sh.run.tcloudbase.com}"
-
-node test-api.js "$SERVICE_URL"
